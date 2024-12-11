7fc31490a134f5e841d5bb289922115dc751748c
ULL4AH <Ull4ah@outlook.com>
2024-12-11 23:46:36 +0600

    Create 🙂.js

---
+++ scripts/cmds/🙂.js
@@ -0,0 +1,22 @@
+module.exports = {
+ config: {
+	 name: "🙂",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "🙂") {
+ return message.reply({
+ body: "     「𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆-----ᑘᒪᒪᗩSᕼ ッ」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/QeOll4M.mp4")
+ });
+ }
+ }
+}
