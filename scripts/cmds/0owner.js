d8cb9d9a7344a5a62cc3997d4364901da2524609
ULL4AH <Ull4ah@outlook.com>
2024-12-11 23:06:53 +0600

    Update 0owner.js

--- scripts/cmds/0owner.js
+++ scripts/cmds/0owner.js
@@ -16,15 +16,15 @@ config: {
   onStart: async function ({ api, event }) {
   try {
     const ownerInfo = {
-      name: 'BAYJID Ai',
+      name: '☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆',
       gender: 'Male',
-      age: '17+',
+      age: '21+',
       height: 'Unknown',
-      facebookLink: 'https://www.facebook.com/BAYJID.404',
-      nick: 'BIJU'
+      facebookLink: 'https://www.facebook.com/ayat.chowdhury.936741',
+      nick: 'ULLASH'
     };
 
-    const bold = 'https://i.imgur.com/DDO686J.mp4'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
+    const bold = 'https://i.imgur.com/zLH9Akq.jpeg'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
 
     const tmpFolderPath = path.join(__dirname, 'tmp');
 
