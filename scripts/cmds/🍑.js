4f292e3c2fdb0862987db31f240bce755dd3b964
ULL4AH <Ull4ah@outlook.com>
2024-12-11 23:52:21 +0600

    Create 🍑.js

---
+++ scripts/cmds/🍑.js
@@ -0,0 +1,22 @@
+module.exports = {
+ config: {
+	 name: "🍑",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "🍑") {
+ return message.reply({
+ body: "     「সরকার বলেছে , সারাদিন লাগাতে 🫦🥵\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆\nᑘᒪᒪᗩSᕼ ッ」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/JD1Z8oS.mp4")
+ });
+ }
+ }
+}
