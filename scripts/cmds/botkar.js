125f4c48258c3854aeba5c1762e7d1fc0f34ee38
Ullashbackup <affarhana4@gmail.com>
2024-12-12 23:38:07 +0600

    Create botkar.js

---
+++ scripts/cmds/botkar.js
@@ -0,0 +1,20 @@
+module.exports = {
+	config: {
+			name: "bot kar",
+			version: "1.0",
+			author: "Jaychris Garcia",
+			countDown: 5,
+			role: 0,
+			shortDescription: "sarcasm",
+			longDescription: "sarcasm",
+			category: "reply",
+	},
+onStart: async function(){}, 
+onChat: async function({
+	event,
+	message,
+	getLang
+}) {
+	if (event.body && event.body.toLowerCase() == "bot kar") return message.reply("-আসসালামুয়ালাইকুম-আমি বট\nআমার বস এর নাম উল্লাস\n\n আমার বসের সম্পর্কে জানতে অথবা বসের ফেসবুক আইড়ি পেতে টাইপ করুন👇\n\n /info");
+}
+};
