1aaba4216dafdb2504886327d2575aee8e3bb009
Ullashbackup <affarhana4@gmail.com>
2024-12-13 02:15:59 +0600

    Update Ullash.js

--- scripts/cmds/Ullash.js
+++ scripts/cmds/Ullash.js
@@ -1,6 +1,6 @@
 module.exports = {
  config: {
-	 name: "ullash",
+	 name: "@ullash ッ",
 	 version: "1.0",
 	 author: "AceGun",
 	 countDown: 5,
@@ -12,7 +12,7 @@ module.exports = {
 
  onStart: async function(){}, 
  onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "ullash") {
+ if (event.body && event.body.toLowerCase() === "@ullash ッ") {
  return message.reply({
  body: " 「 🥰❥︎-আমি ভেঙেচুরে নিজে কে আবার গড়তে চাই।\n❥︎-কিছু মানুষ কে তাদের যুগ্গ জবাব দিতে চাই।࿐.🌴🤎🙂.\n❥ যারা আমাকে তুচ্ছ ভেবে অবহেলা করসে তাদের আফসোস হতে চাই 😅\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\ᑘᒪᒪᗩSᕼ ッ」",
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/LSqpE9c.mp4")
