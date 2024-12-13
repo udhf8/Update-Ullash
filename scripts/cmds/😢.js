34a1c6f42488ddf31972b8af147698c8eee283aa
Ullashbackup <affarhana4@gmail.com>
2024-12-13 09:31:29 +0600

    Create 😢.js

---
+++ scripts/cmds/😢.js
@@ -0,0 +1,22 @@
+module.exports = {
+ config: {
+	 name: "😢",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "😢") {
+ return message.reply({
+ body: "     「صلی الله علیه وآله وسلم\n\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆\nᑘᒪᒪᗩSᕼ ッ」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/bFz6DHz.mp4")
+ });
+ }
+ }
+}
