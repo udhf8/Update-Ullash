391ec9c7b9bf5685502d0d7c574a6641958b6a1b
Ullashbackup <affarhana4@gmail.com>
2024-12-18 13:19:10 +0600

    Create 🤤.js

---
+++ scripts/cmds/🤤.js
@@ -0,0 +1,22 @@
+module.exports = {
+ config: {
+	 name: "🤤",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "🤤") {
+ return message.reply({
+ body: "     「  \n\n\n 𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆\n\nᑘᒪᒪᗩSᕼ ッ」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/mShdbYA.mp4")
+ });
+ }
+ }
+}
