fb0da661d79dc3bf4b2474e76959a24a80d94fdd
Ullashbackup <affarhana4@gmail.com>
2024-12-26 20:18:06 +0600

    Update Bot.js

--- scripts/cmds/Bot.js
+++ scripts/cmds/Bot.js
@@ -10,7 +10,7 @@ module.exports.config = {
     return credit;
   })(),
   description: "Sends random quotes with an image",
-  commandCategory: "𝗜𝗦𝗟𝗔𝗠",
+  commandCategory: "caption",
   usages: "bot",
   cooldowns: 11,
   dependencies: {
