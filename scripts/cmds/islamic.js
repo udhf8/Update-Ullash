c9d6f862df66d4f216c6d5b81af7d0dafda2a430
Ullashbackup <affarhana4@gmail.com>
2025-01-01 01:45:19 +0600

    Update islamic.js

--- scripts/cmds/islamic.js
+++ scripts/cmds/islamic.js
@@ -2,7 +2,7 @@ module.exports = {
   config: {
     name: "islamic",
     version: "7.0",
-    author: "BAYJID",
+    author: "Ullash ッ",
     countDown: 5,
     role: 0,
     shortDescription: "all video ðŸ“·",
@@ -14,6 +14,15 @@ module.exports = {
    var BAYJID= ["https://i.imgur.com/FbnZI40.mp4",
 "https://i.imgur.com/8k6OOZg.mp4",
 "https://i.imgur.com/lgQghHX.mp4",
+"https://i.imgur.com/D7HZFSg.mp4"
+"https://i.imgur.com/vUe9Zlv.mp4"
+"https://i.imgur.com/oxFuJYw.mp4"
+"https://i.imgur.com/OKKlDBN.mp4"
+"https://i.imgur.com/6wWebFc.mp4"
+"https://i.imgur.com/K2LTmaA.mp4"
+"https://i.imgur.com/i9vKvTd.mp4"
+"https://i.imgur.com/Y6uBzxx.mp4"
+"https://i.imgur.com/ULtFVPQ.mp4"
 "https://i.imgur.com/wX8WJh3.mp4",
 "https://i.imgur.com/6A42EIx.mp4",
 "https://i.imgur.com/ozRevxt.mp4",
@@ -22,7 +31,7 @@ module.exports = {
 "https://i.imgur.com/sDNohv4.mp4",
 "https://i.imgur.com/JBu2Ie3.mp4",
 "https://i.imgur.com/UaY42rq.mp4",
- "https://i.imgur.com/NFxf731.mp4",
+"https://i.imgur.com/NFxf731.mp4",
 "https://i.imgur.com/vv1HsMC.mp4",
 "https://i.imgur.com/Y8MPzLv.mp4",
 "https://i.imgur.com/9M1v1qK.mp4",
